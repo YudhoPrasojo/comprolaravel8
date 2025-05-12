7417f2c06c82d0e7f3ac49561554102e2d5cacbd
YudhoPrasojo <64570534+YudhoPrasojo@users.noreply.github.com>
2025-05-12 13:35:30 +0800

    post

---
+++ resources/views/layout/new/js.blade.php
@@ -0,0 +1,12 @@
+<!-- Vendor JS Files -->
+  <script src="{{ asset('/assets_dewi/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('/assets_dewi/assets/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ asset('/assets_dewi/assets/vendor/aos/aos.js') }}"></script>
+  <script src="{{ asset('/assets_dewi/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ asset('/assets_dewi/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+  <script src="{{ asset('/assets_dewi/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset('/assets_dewi/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+  <script src="{{ asset('/assets_dewi/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+
+  <!-- Main JS File -->
+  <script src="{{ asset('/assets_dewi/assets/js/main.js') }}"></script>
